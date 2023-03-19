09d4bacc5b0b07cc9b35aefaee74ef09c6af4563
YouSeungHak <ch10429@naver.com>
2023-03-19 21:51:09 +0900

    feat(1971_FindIfPathExistsInGraph): Solution FindIfPathExistsInGraph
    1. _1971_Node 클래스 : 그래프 노드 클래스르 만들어서 이름, 노드리스트, 방문여부를 멤버로 만든다.
    2. CreateGraph 함수 : 전달 받은 n과 edges로 Map 자료구조 Graph를 만든다.
    3. isPathSourceToDestinationInBFS 함수 : 그래프를 BFS 방식으로 탐색을 한다. source 부터 출발해서 destinatin과 만나는 지점이 있으면 return true를 하고 없으면 return false를 한다.

--- src/_1900_2000/_1971_FindIfPathExistsInGraph.java
+++ src/_1900_2000/_1971_FindIfPathExistsInGraph.java
@@ -1,8 +1,69 @@
 package _1900_2000;
 
+import java.util.*;
+
+class _1971_Node {
+    int nodeName;
+    ArrayList<_1971_Node> nodeList = new ArrayList<>();
+    boolean visited;
+
+    _1971_Node(int n) {
+        nodeName = n;
+        visited = false;
+    }
+    public void ConnectNode(_1971_Node node) {
+        this.nodeList.add(node);
+        node.nodeList.add(this);
+    }
+}
+
 public class _1971_FindIfPathExistsInGraph {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+    boolean isPathSourceToDestinationInBFS(Map<Integer, _1971_Node> graph, int source, int destination) {
+        // destination 값을 만나면 리턴 true 못 만나면 return false
+        Queue<_1971_Node> queue = new LinkedList<>();
+
+        graph.get(source).visited = true;
+        queue.add(graph.get(source));
+
+        while (queue.size() > 0) {
+            _1971_Node curNode = queue.poll();
+            for(int i = 0; i < curNode.nodeList.size(); i++) {
+                if(curNode.nodeList.get(i).visited == false) {
+                    if(curNode.nodeList.get(i).nodeName == destination)
+                        return true;
+
+                    curNode.nodeList.get(i).visited = true;
+                    queue.add(curNode.nodeList.get(i));
+                }
+            }
+        }
 
         return false;
+    }
+
+    public Map<Integer, _1971_Node> CreateGraph(int n, int[][] edges) {
+        Map<Integer, _1971_Node> graph = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            _1971_Node node = new _1971_Node(i);
+            graph.put(i, node);
+        }
+
+        for(int i = 0; i < edges.length; i++) {
+            int n1 = edges[i][0];
+            int n2 = edges[i][1];
+
+            graph.get(n1).ConnectNode(graph.get(n2));
+        }
+
+        return graph;
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(n == 1 || source == destination)
+            return true;
+
+        Map<Integer, _1971_Node> graph = CreateGraph(n, edges);
+        return isPathSourceToDestinationInBFS(graph, source, destination);
     }
 }
