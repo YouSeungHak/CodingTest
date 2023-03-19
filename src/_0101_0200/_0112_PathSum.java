0268344c9cde900b45712a984cefe591ed5207f8
YouSeungHak <ch10429@naver.com>
2023-03-19 20:27:47 +0900

    feat(0112_PathSum): Solution PathSum
    - pre-order 순회 방식으로
    1. node가 null이면 리턴
    2. 현재 노드의 값을 더해준다.
    3. 현재 노드의 왼쪽과 오른쪽에 노드가 없는지 확인(leaf Node)해서 없으면 요청한 target 값과 더한 target 값을 비교 하여 같으면 리턴 값을 true로 바꿔준다.
    4. 더한 값과 요청한 target 값이 같은게 없으면 false로 리턴한다.

--- src/_0101_0200/_0112_PathSum.java
+++ src/_0101_0200/_0112_PathSum.java
@@ -1,7 +1,24 @@
 package _0101_0200;
 
 public class _0112_PathSum {
+    int targetSum = 0;
+    boolean ret = false;
+    public void plusPathSum(_0112_TreeNode node, int targetSum) {
+        if(node == null) {
+            return;
+        }
+        this.targetSum += node.val;
+        if(node.left == null && node.right == null) {
+            if(this.targetSum == targetSum)
+                this.ret = true;
+        }
+        plusPathSum(node.left, targetSum);
+        plusPathSum(node.right, targetSum);
+        this.targetSum -= node.val;
+    }
+
     public boolean hasPathSum(_0112_TreeNode root, int targetSum) {
-        return false;
+        plusPathSum(root, targetSum);
+        return this.ret;
     }
 }
