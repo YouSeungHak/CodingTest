06b43ba9516f82fbe0ef6dad46be6dd19c2893a3
YouSeungHak <ch10429@naver.com>
2023-03-20 18:05:39 +0900

    refact0112_PathSum): 코드 리팩터링
    - targeSum 체크하는 루틴 함수화
    - 리턴할 때 this.ret와 node == null을 합침

--- src/_0101_0200/_0112_PathSum.java
+++ src/_0101_0200/_0112_PathSum.java
@@ -3,16 +3,24 @@ package _0101_0200;
 public class _0112_PathSum {
     int targetSum = 0;
     boolean ret = false;
-    public void plusPathSum(_0112_TreeNode node, int targetSum) {
-        if(this.ret)
-            return;
-        if(node == null)
-            return;
-        this.targetSum += node.val;
-        if(node.left == null && node.right == null) {
+
+    private boolean isLeafNode(_0112_TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+
+    private void checkTargetSum(_0112_TreeNode node, int targetSum) {
+        if(isLeafNode(node)) {
             if(this.targetSum == targetSum)
                 this.ret = true;
         }
+    }
+
+    private void plusPathSum(_0112_TreeNode node, int targetSum) {
+        if(this.ret || node == null)
+            return;
+
+        this.targetSum += node.val;
+        checkTargetSum(node, targetSum);
         plusPathSum(node.left, targetSum);
         plusPathSum(node.right, targetSum);
         this.targetSum -= node.val;
