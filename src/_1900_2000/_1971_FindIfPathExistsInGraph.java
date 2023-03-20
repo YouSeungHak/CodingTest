94aa56635df90602751362e726294aee87bab126
YouSeungHak <ch10429@naver.com>
2023-03-20 18:04:31 +0900

    refact(1971_FindIfPathExistsInGraph): 코드 리팩터링
    1. 변수 이름 변경
     - _1971_Node 클래스 : nodeName -> nodeNum
    2. 함수 이름 변경
     - _1971_Node 클래스 : ConnectNode -> connectNode
     - _1971_FindIfPathExistsInGraph 클래스 : IsPathSourceToDestinationInBFS -> isPathSourceToDestinationBFS
     - _1971_FindIfPathExistsInGraph 클래스 : CreateGraph -> createGraph
    3. 소스 코드 간소화
     - 노드 방문할 때 false를 !로 변경
     - 그래프 만들 때 for문 각각 1줄로 간소화

--- src/_1900_2000/_1971_FindIfPathExistsInGraph.java
+++ src/_1900_2000/_1971_FindIfPathExistsInGraph.java
@@ -3,24 +3,22 @@ package _1900_2000;
 import java.util.*;
 
 class _1971_Node {
-    int nodeName;
+    int nodeNum;
     ArrayList<_1971_Node> nodeList = new ArrayList<>();
     boolean visited;
 
     _1971_Node(int n) {
-        nodeName = n;
+        nodeNum = n;
         visited = false;
     }
-    public void ConnectNode(_1971_Node node) {
+    public void connectNode(_1971_Node node) {
         this.nodeList.add(node);
         node.nodeList.add(this);
     }
 }
 
 public class _1971_FindIfPathExistsInGraph {
-
-    boolean IsPathSourceToDestinationInBFS(Map<Integer, _1971_Node> graph, int source, int destination) {
-        // destination 값을 만나면 리턴 true 못 만나면 return false
+    boolean isPathSourceToDestinationInBFS(Map<Integer, _1971_Node> graph, int source, int destination) {
         Queue<_1971_Node> queue = new LinkedList<>();
 
         graph.get(source).visited = true;
@@ -29,8 +27,8 @@ public class _1971_FindIfPathExistsInGraph {
         while (queue.size() > 0) {
             _1971_Node curNode = queue.poll();
             for(int i = 0; i < curNode.nodeList.size(); i++) {
-                if(curNode.nodeList.get(i).visited == false) {
-                    if(curNode.nodeList.get(i).nodeName == destination)
+                if(!curNode.nodeList.get(i).visited) {
+                    if(curNode.nodeList.get(i).nodeNum == destination)
                         return true;
 
                     curNode.nodeList.get(i).visited = true;
@@ -42,18 +40,14 @@ public class _1971_FindIfPathExistsInGraph {
         return false;
     }
 
-    public Map<Integer, _1971_Node> CreateGraph(int n, int[][] edges) {
+    private Map<Integer, _1971_Node> createGraph(int n, int[][] edges) {
         Map<Integer, _1971_Node> graph = new HashMap<>();
         for(int i = 0; i < n; i++) {
-            _1971_Node node = new _1971_Node(i);
-            graph.put(i, node);
+            graph.put(i, new _1971_Node(i));
         }
 
-        for(int i = 0; i < edges.length; i++) {
-            int n1 = edges[i][0];
-            int n2 = edges[i][1];
-
-            graph.get(n1).ConnectNode(graph.get(n2));
+        for (int[] edge : edges) {
+            graph.get(edge[0]).connectNode(graph.get(edge[1]));
         }
 
         return graph;
@@ -63,7 +57,7 @@ public class _1971_FindIfPathExistsInGraph {
         if(source == destination)
             return true;
 
-        Map<Integer, _1971_Node> graph = CreateGraph(n, edges);
-        return IsPathSourceToDestinationInBFS(graph, source, destination);
+        Map<Integer, _1971_Node> graph = createGraph(n, edges);
+        return isPathSourceToDestinationInBFS(graph, source, destination);
     }
 }
