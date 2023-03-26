9311037415ec864d197e6bd7a43c9d013f160fea
YouSeungHak <ch10429@naver.com>
2023-03-26 17:23:46 +0900

    feat(0881_BoatsToSavePeople): ArrayList로 구현, 하지만 테스트 케이스 72번에서 TimeOut 발생

--- src/_0801_0900/_0881_BoatsToSavePeople.java
+++ src/_0801_0900/_0881_BoatsToSavePeople.java
@@ -1,7 +1,62 @@
 package _0801_0900;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class _0881_BoatsToSavePeople {
+//    public int numRescueBoats(int[] people, int limit) {
+//        int count = 0;
+//        Map<Integer, Integer> map = new HashMap<>();
+//
+//        Arrays.sort(people);
+//        for (int i = 0; i < people.length; i++) {
+//            map.put(i, people[i]);
+//        }
+//
+//        for (int i = (people.length - 1); i >= 0; i--) {
+//            if(map.get(i) < 0)
+//                continue;
+//            if(people[i] < limit)
+//            {
+//                for (int j = (i - 1); j >= 0; j--) {
+//                    if(map.get(j) < 0)
+//                        continue;
+//
+//                    if(people[i] + people[j] <= limit)
+//                    {
+//                        map.put(j, -1);
+//                        break;
+//                    }
+//                }
+//            }
+//
+//            count++;
+//        }
+//
+//        return count;
+//    }
+
     public int numRescueBoats(int[] people, int limit) {
-        return 0;
+        int count = 0;
+
+        ArrayList<Integer> peopleList = new ArrayList<>();
+        for (int weight : people) {
+            peopleList.add(weight);
+        }
+        Collections.sort(peopleList, Collections.reverseOrder());
+
+        while(count < peopleList.size()) {
+            if(peopleList.get(count) < limit) {
+                for (int j = count + 1; j < peopleList.size(); j++) {
+                    if (peopleList.get(count) + peopleList.get(j) <= limit) {
+                        peopleList.remove(j);
+                        break;
+                    }
+                }
+            }
+            count++;
+        }
+
+        return count;
     }
 }
