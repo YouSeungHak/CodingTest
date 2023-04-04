c68d743faf576fe87ee65e78da42f92995c49785
YouSeungHak <ch10429@naver.com>
2023-04-04 18:17:52 +0900

    refact(0225_ImplementStackUsingQueues): 널 가능 연산자 제거

--- src/_0201_0300/Kt0225ImplementStackUsingQueues.kt
+++ src/_0201_0300/Kt0225ImplementStackUsingQueues.kt
@@ -4,47 +4,47 @@ import java.util.*
 
 class Kt0225ImplementStackUsingQueues {
     private var posCurQueue = 0
-    private var queue1: Queue<Int>? = LinkedList()
-    private var queue2: Queue<Int>? = LinkedList()
+    private var queue1: Queue<Int> = LinkedList()
+    private var queue2: Queue<Int> = LinkedList()
 
     private fun changeQueue() {
         posCurQueue = if (posCurQueue == 0) 1 else 0
     }
 
-    private fun curQueue(): Queue<Int>? {
+    private fun curQueue(): Queue<Int> {
         return if (posCurQueue == 0) queue1 else queue2
     }
 
-    private fun backupQueue(): Queue<Int>? {
+    private fun backupQueue(): Queue<Int> {
         return if (posCurQueue == 0) queue2 else queue1
     }
 
     fun push(x: Int) {
-        curQueue()!!.add(x)
+        curQueue().add(x)
     }
 
     fun pop(): Int {
-        if (curQueue()!!.size == 0) return 0
-        while (curQueue()!!.size > 1) {
-            backupQueue()!!.add(curQueue()!!.poll())
+        if (curQueue().size == 0) return 0
+        while (curQueue().size > 1) {
+            backupQueue().add(curQueue().poll())
         }
-        val retData = curQueue()!!.poll()
+        val retData = curQueue().poll()
         changeQueue()
         return retData
     }
 
     fun top(): Int {
-        if (curQueue()!!.size == 0) return 0
-        while (curQueue()!!.size > 1) {
-            backupQueue()!!.add(curQueue()!!.poll())
+        if (curQueue().size == 0) return 0
+        while (curQueue().size > 1) {
+            backupQueue().add(curQueue().poll())
         }
-        val retData = curQueue()!!.poll()
-        backupQueue()!!.add(retData)
+        val retData = curQueue().poll()
+        backupQueue().add(retData)
         changeQueue()
         return retData
     }
 
     fun empty(): Boolean {
-        return curQueue()!!.size == 0
+        return curQueue().size == 0
     }
 }
